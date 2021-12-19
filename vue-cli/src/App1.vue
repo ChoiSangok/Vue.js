dbb22c5ee00a093a357c80f1ac28bf624763dfb4
ChoiSangOk <tkddhr12345@naver.com>
2021-12-19 21:05:50 +0900

    [update] vue-cli 기초

---
+++ vue-cli/src/App1.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--  -->
+    <hello-world></hello-world> <!-- 공식 스타일 가이드 -->
+    <HelloWorld></HelloWorld>
+    <HelloWord/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    'hello-world': HelloWorld,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- vue-cli/src/main.js
+++ vue-cli/src/main.js
@@ -1,8 +1,31 @@
 import Vue from 'vue'
-import App from './App.vue'
+
+// 여기보기전에
+// import App from './App.vue'
 
 Vue.config.productionTip = false
 
+
+var App = {
+  template: '<div>app</div>'
+}
 new Vue({
   render: h => h(App),
+  components: {
+    'app': App
+  }
 }).$mount('#app')
+
+
+
+// new Vue({
+//   el: "#app",
+//   render: h => h(App),
+// })
+
+
+
+
+// new Vue){
+//   el: "#app"
+// }
