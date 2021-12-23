fc98bcdb405979e71302b6beb706a8e8f6fefa59
ChoiSangOk <tkddhr12345@naver.com>
2021-12-23 21:58:36 +0900

    [update] cli components

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,15 +1,24 @@
 <template>
   <div>
-      {{ str }}
+      <!-- <app-header v-bind:프롭스 속성이름="상위 컴포넌트의 데이터 이름"></app-header> -->
+      <app-header v-bind:propsdata="str"></app-header>
   </div>
 </template>
 
 <script>
+import AppHeader from './components/AppHeader.vue';
+
+var AppHeader = {
+    template: '<header>Header</header>'
+}
 export default {
     data: function() {
         return {
-            str: 'hi'
+            str: 'header'
         }
+    },
+    components: {
+        'app-header': AppHeader
     }
 }
 </script>
